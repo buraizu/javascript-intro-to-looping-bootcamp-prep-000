a33e0dda21a8a004d701bf17d1c09327f8e8a632
whoozit.whatsit@gmail.com <whoozit.whatsit@gmail.com>
2018-07-30 19:51:54 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,21 @@
+function forLoop (array) {
+  for (let i =0; i < 25; i++) {
+    if (i === 1) {
+    array.push(`I am ${i} strange loop`);
+    } else {
+      array.push(`I am ${i} strange loops`);
+    }
+    
+  }
+  return array;
+}
+
+function whileLoop (number) {
+  while (number > 0) {
+    --number;
+    console.log(number);
+    if (number === 0) {
+      return 'done';
+    }
+  }
+}
